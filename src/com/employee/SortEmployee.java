ca727d0499f164e5853813f206d54f833f2ec2e7
Yasowant <yasowant1998@gmail.com>
2023-06-16 12:43:39 +0530

    random filesdd

--- src/com/employee/SortEmployee.java
+++ src/com/employee/SortEmployee.java
@@ -2,6 +2,8 @@ package com.employee;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
+import java.util.Scanner;
 
 public class SortEmployee {
 	public static void main(String[] args) {
@@ -17,11 +19,33 @@ public class SortEmployee {
 		t.add(e2);
 		t.add(new Employee(23, "Girish", 35.69));
 		t.add(new Employee(21, "Mayank", 15.692));
-		
+
 //		 Collections.sort(t, new SortEmployeeById());
 //		 Collections.sort(t, new SortEmployeeByName());
-		 Collections.sort(t, new SortEmployeeSalary());
-		 
+//		 Collections.sort(t, new SortEmployeeSalary());
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter the sorting criteria (id/name/salary): ");
+		String sortingCriteria = scanner.nextLine().toLowerCase();
+
+		Comparator<Employee> comparator;
+
+		switch (sortingCriteria) {
+		case "id":
+			comparator = new SortEmployeeById();
+			break;
+		case "name":
+			comparator = new SortEmployeeByName();
+			break;
+		case "salary":
+			comparator = new SortEmployeeSalary();
+			break;
+		default:
+			System.out.println("Invalid sorting criteria. Sorting by ID.");
+			comparator = new SortEmployeeById();
+			break;
+		}
+
+		Collections.sort(t, comparator);
 
 		for (Employee e : t) {
 			System.out.println(e);
